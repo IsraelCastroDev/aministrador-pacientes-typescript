06f6b2df8f3b5288d6d8cf4f78822accf5cd20f2
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-03 18:30:56 -0500

    Agregando las notoficaciones

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
-import { usePatientStore } from "../store";
+import { toast } from "react-toastify";
+import { usePatientStore } from "../store/store";
 import type { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
 
@@ -8,6 +9,7 @@ type Props = {
 
 function PatientDetails({ patient }: Props) {
   const deletePatient = usePatientStore((state) => state.deletePatient);
+  const getPatientById = usePatientStore((state) => state.getPatientById);
 
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -20,6 +22,7 @@ function PatientDetails({ patient }: Props) {
 
       <div className="flex justify-between mt-10">
         <button
+          onClick={() => getPatientById(patient.id)}
           type="button"
           className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg"
         >
@@ -27,7 +30,10 @@ function PatientDetails({ patient }: Props) {
         </button>
 
         <button
-          onClick={() => deletePatient(patient.id)}
+          onClick={() => {
+            deletePatient(patient.id);
+            toast.success("Paciente eliminado");
+          }}
           type="button"
           className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-lg"
         >
