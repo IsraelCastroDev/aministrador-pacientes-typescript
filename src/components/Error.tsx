c797eb131ac66e7027feec1eb44b85e0537ea9e3
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-03 17:26:01 -0500

    Creando comoponente de Error

---
+++ src/components/Error.tsx
@@ -0,0 +1,9 @@
+function Error({ children }: { children: React.ReactNode }) {
+  return (
+    <p className="text-center my-4 bg-red-600 font-bold p-3 uppercase text-sm text-white">
+      {children}
+    </p>
+  );
+}
+
+export default Error;
