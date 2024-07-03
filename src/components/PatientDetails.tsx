bd435825d08f4b4d39a7ea004b6dc2011afc4047
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-03 17:40:18 -0500

    Agregando los campos faltantes

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,3 +1,4 @@
+import { usePatientStore } from "../store";
 import type { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
 
@@ -6,10 +7,33 @@ type Props = {
 };
 
 function PatientDetails({ patient }: Props) {
+  const deletePatient = usePatientStore((state) => state.deletePatient);
+
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
       <PatientDetailItem label="ID" text={patient.id} />
       <PatientDetailItem label="Nombre" text={patient.name} />
+      <PatientDetailItem label="Propietario" text={patient.caretaker} />
+      <PatientDetailItem label="Email" text={patient.email} />
+      <PatientDetailItem label="Date" text={patient.date.toString()} />
+      <PatientDetailItem label="Síntomas" text={patient.symptoms} />
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold rounded-lg"
+        >
+          Editar
+        </button>
+
+        <button
+          onClick={() => deletePatient(patient.id)}
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 }
