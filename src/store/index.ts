222381972acf8272830046a8e678503dd29b9499
Isreal Castro <israelcastro.dev@gmail.com>
2024-07-03 17:26:18 -0500

    Creando el store de zustand

---
+++ src/store/index.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import type { DraftPatient, Patient } from "../types";
+
+type PatientState = {
+  patients: Patient[];
+  addPatient: (data: DraftPatient) => void;
+};
+
+const createPatient = (patient: DraftPatient): Patient => {
+  return { ...patient, id: crypto.randomUUID() };
+};
+
+export const usePatientStore = create<PatientState>((set) => ({
+  patients: [],
+  addPatient: (data) => {
+    const newPatient = createPatient(data);
+
+    set((state) => ({
+      patients: [...state.patients, newPatient],
+    }));
+  },
+}));
